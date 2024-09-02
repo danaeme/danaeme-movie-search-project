15967de660043f3e4b36836ad6cee803e425dbf0
David Anaeme <davidcanaeme@gmail.com>
2024-09-01 23:27:36 -0600

    Search routes added

--- controllers/users.js
+++ controllers/users.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const bcrypt = require('bcrypt');
 const User = require('../models/user');
 const jwt = require('jsonwebtoken');
+const Movie = require('../models/movie');
+const verifyToken = require('../middleware/verify-token');
 
 
 const SALT_LENGTH = 12;
@@ -13,12 +15,10 @@ const SALT_LENGTH = 12;
 
 router.post('/signup', async (req, res) => {
     try {
-        // Check if the username is already taken
         const userInDatabase = await User.findOne({ username: req.body.username });
         if (userInDatabase) {
             return res.json({error: 'Username already taken.'});
         }
-        // Create a new user with hashed password
         const user = await User.create({
             username: req.body.username,
             email: req.body.email,
@@ -44,6 +44,40 @@ router.post('/signin', async (req, res) => {
         }
     } catch (error) {
         res.status(400).json({ error: error.message });
+    }
+});
+
+// route to search for users
+router.get('/search', verifyToken, async (req, res) => {
+    const { query } = req.query;
+    try {
+        const users = await User.find({
+            $or: [
+                { username: { $regex: query, $options: 'i' } },
+                { email: { $regex: query, $options: 'i' } }
+            ]
+        }).select('username email');
+        res.status(200).json(users);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+// GET user profile with entries
+router.get('/:userId', verifyToken, async (req, res) => {
+    try {
+        const user = await User.findById(req.params.userId)
+            .populate('movies') 
+            .populate({
+                path: 'movies',
+                populate: { path: 'comments' } // Populate comments
+            });
+        if (!user) {
+            return res.status(404).json({ error: 'User not found.' });
+        }
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
     }
 });
 
